d97ec39f09f045c0f9531d3bfa07117e3612dab6
GlobalVentures.com <maidabarrientos@gmail.com>
2023-10-04 13:28:10 +0800

    Committing all

---
+++ components/Forms/contact.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Script from 'next/script';
+
+const VNOC_contact: React.FC = () => {
+  return (
+    <div>
+       
+        <div className="widget2">
+        <Script src="https://tools.contrib.com/pages/contact?d=vnoc.com&class=widget2" />
+        </div>
+    </div>
+  );
+};
+
+export default VNOC_contact;
